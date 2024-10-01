9b2880c70fd77d86f67c13b600f06a9b5b4f8ee8
Anthony mwaura <antony.mwaura@student.moringaschool.comc0deanthony@anthony-HP-EliteBook-840-G5:~/f11$ git config --global user.email antony.mwaura@student.moringaschool.com>
2024-10-01 16:21:16 +0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+// Function to calculate total batteries
+const totalBatteries = batteryBatches.reduce((sum, batch) => sum + batch, 0);
+
+// Output the total number of batteries
+console.log(`Total Batteries: ${totalBatteries}`);
+
+module.exports = {
+  totalBatteries,
+};
